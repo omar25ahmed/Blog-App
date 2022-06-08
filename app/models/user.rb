c4a23973774742a9993bab9d71dced42a4c95fd1
Omar Ragheb <ahmed.raph29@gmail.com>
2022-06-08 06:03:19 +0200

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :comments, class_name: 'Comment', foreign_key: 'user_id'
   has_many :likes, class_name: 'Like', foreign_key: 'user_id'
 
-  def most_recent_post
+  def most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
