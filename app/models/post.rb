725e74230b2a16a0748c21f64d9170d0900eb86b
Omar Ragheb <ahmed.raph29@gmail.com>
2022-06-10 05:42:32 +0200

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ActiveRecord::Base
   has_many :comments, class_name: 'Comment'
   after_save :update_post_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
   def update_post_counter
     user.increment!(:posts_counter)
   end
